aa8ee15758f7d27fd9e5411329ec088e424a3934
Ivan Vekhov <Ivan.Vekhov@mediacom.com>
2018-12-30 23:56:43 +0300

    Add work files

---
+++ server.py
@@ -0,0 +1,42 @@
+from bottle import run, route, view
+from datetime import datetime as dt
+from random import random
+from horoscope.horoscope import generate_prophecies
+import os
+
+
+
+@route("/")
+def index():
+    return "<h1>Success!</h1>"
+
+
+@route("/check")
+@view("predictions")
+def index():
+	now = dt.now()
+	x = random()
+	predictions = generate_prophecies(5, 3)
+
+	return {
+		"date": f"{now.year}-{now.month}-{now.day}", 
+		"predictions": predictions,
+		"special_date": x > 0.5, 
+		"x": x,	
+	}
+
+
+@route("/api/test")
+def api_test():
+	return {"test_passed": True}
+
+
+@route("/api/forecast")
+def api_forecast():
+	pass
+
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+	run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+	run(host='localhost', port=8080, debug=True)
